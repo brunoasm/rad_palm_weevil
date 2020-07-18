053baa4f69301a555d7f62a553fa951a95b55eb5
Bruno de Medeiros <souzademedeiros@fas.harvard.edu>
2020-07-18 11:52:06 -0400

    added hierarchical model for sp interactions

--- bedassle2/6-slurm_bedassle.sh
+++ bedassle2/6-slurm_bedassle.sh
@@ -2,7 +2,7 @@
 #SBATCH -n 4                # Number of cores
 #SBATCH -N 1                # Ensure that all cores are on one machine
 #SBATCH -t 0-8:00          # Runtime in D-HH:MM, minimum of 10 minutes
-#SBATCH -p shared,huce_cascade,test    # Partition to submit to
+#SBATCH -p shared,test    # Partition to submit to
 #SBATCH --mem=1000           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o bedassle.%a.%A.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -J bedassle
